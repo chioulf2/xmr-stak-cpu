a7dacef3297a2433b2706f99c6e09921a8c18688
Jimmy <chioulf2@Yahoo.com.tw>
2017-06-18 05:38:26 +0000

    fix

--- install.sh
+++ install.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-sudo apt-get install aptitude << y <<
-sudo aptitude update << y <<
-sudo aptitude install –with-recommends build-essential autotools-dev autoconf automake libcurl3 libcurl4-gnutls-dev git make cmake libssl-dev pkg-config libevent-dev libunbound-dev libminiupnpc-dev doxygen supervisor jq libboost-all-dev htop libmicrohttpd-dev <<y<<
+sudo apt-get install aptitude 
+echo "y"
+sleep 1
+sudo aptitude update
+exho "y"
+sleep 1
+sudo aptitude install –with-recommends build-essential autotools-dev autoconf automake libcurl3 libcurl4-gnutls-dev git make cmake libssl-dev pkg-config libevent-dev libunbound-dev libminiupnpc-dev doxygen supervisor jq libboost-all-dev htop libmicrohttpd-dev 
 cd bin
 sudo sysctl -w vm.nr_hugepages=128
 
